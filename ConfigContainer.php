a753b47940a214988bca9ee87004f588cf46666f
Joshua Parker <joshua@joshuaparker.dev>
2026-02-11 09:33:52 -0800

    Added inheritance library to implement ValueType contract.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

--- ConfigContainer.php
+++ ConfigContainer.php
@@ -14,9 +14,9 @@ declare(strict_types=1);
 namespace Qubus\Config;
 
 use Qubus\Exception\Data\TypeException;
-use Qubus\Exception\Exception;
+use Qubus\Inheritance\Contract\ValueType;
 
-interface ConfigContainer
+interface ConfigContainer extends ValueType
 {
     /**
      * Get an item from current configuration.
@@ -41,54 +41,4 @@ interface ConfigContainer
      * Checks if a key exists.
      */
     public function hasConfigKey(string $key): bool;
-
-    /**
-     * Get the specified string configuration value.
-     *
-     * @param string $key
-     * @param (\Closure():(string|null))|string|null  $default
-     * @return string
-     * @throws TypeException
-     */
-    public function string(string $key, mixed $default = null): string;
-
-    /**
-     * Get the specified integer configuration value.
-     *
-     * @param string $key
-     * @param (\Closure():(int|null))|int|null  $default
-     * @return int
-     * @throws TypeException
-     */
-    public function integer(string $key, mixed $default = null): int;
-
-    /**
-     * Get the specified float configuration value.
-     *
-     * @param string $key
-     * @param (\Closure():(float|null))|float|null  $default
-     * @return float
-     * @throws TypeException
-     */
-    public function float(string $key, mixed $default = null): float;
-
-    /**
-     * Get the specified boolean configuration value.
-     *
-     * @param string $key
-     * @param (\Closure():(bool|null))|bool|null  $default
-     * @return bool
-     * @throws TypeException
-     */
-    public function boolean(string $key, mixed $default = null): bool;
-
-    /**
-     * Get the specified array configuration value.
-     *
-     * @param string $key
-     * @param (\Closure():(array<array-key, mixed>|null))|array<array-key, mixed>|null  $default
-     * @return array
-     * @throws TypeException
-     */
-    public function array(string $key, mixed $default = null): array;
 }
